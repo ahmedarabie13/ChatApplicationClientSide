3306ad7fad899a2734e4e46554860066b70a61b5
Ebrahim Mohamed <ebrahim.elmohamed26@gmail.com>
2021-01-29 14:59:57 +0200

    Project intiation

---
+++ src/main/java/eg/gov/iti/jets/Main.java
@@ -0,0 +1,7 @@
+package eg.gov.iti.jets;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Chat Testing");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,23 @@
+module ChatApplication {
+
+    requires javafx.base;
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires org.kordamp.bootstrapfx.core;
+
+    requires org.kordamp.ikonli.core;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.ikonli.fontawesome5;
+    requires org.kordamp.ikonli.materialdesign2;
+
+    requires commons.validator;
+    requires java.sql;
+
+    requires mysql.connector.java;
+    requires java.naming;
+
+    exports eg.gov.iti.jets;
+    opens eg.gov.iti.jets.ui.controllers;
+}
